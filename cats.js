41d1a51bec4a0b3d10d78147aa1e217839c7f9c8
sylvia villegas <sylvia@svillegas.com>
2017-02-18 16:47:56 -0500

    Send AJAX request for cats on click

--- cats.js
+++ cats.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
 
+  var catIndex = 0;
+
   $('.summon-cats').on('click', function() {
-    console.log('summoned!');
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET'
+    }).done(function(catData) {
+      var catList = catData.cats;
+      var cat = catList[catIndex];
+      var catDivNumber = (catIndex + 1).toString();
+      
+      $('<img>').attr('src', cat.photo)
+                .attr('alt', 'Photo of ' + cat.name)
+                .appendTo('#cat' + catDivNumber );
+      catIndex++;
+    });
   });
 
 });
